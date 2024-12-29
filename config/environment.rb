660cd02f4a97c374661f7e21eb1abd4fe369dfc0
Andres Lopez <andreslopezher@gmail.com>
2024-12-28 21:55:50 -0500

    Avoid adding field_with_errors div (when validation occurs)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Disable rails from automatically adding the "field_with_erros" div to fields when validation errors occur,
+# because the fields are visually moved.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
