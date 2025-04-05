ee2a1996445d781e83d55b2ae441db8bce72c6e1
Andres Lopez <andreslopezher@gmail.com>
2025-04-04 23:17:34 -0400

    Create a new session controller to handle user login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #log_in user
+      #remember user
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome back, #{user.username}!"
+      redirect_to user
+    else
+      flash.now[:alert] = 'There was something wrong with your login details.'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'You have been logged out.'
+    redirect_to root_path
+  end
+
+end
